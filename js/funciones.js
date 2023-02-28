26bb6284c12ca9336c84daaa5bc71c0dabf29467
German Moreno <ger.moreno36@gmail.com>
2023-02-28 12:11:10 -0300

    Agregados botones de webs, mostrar y ocultar datos en index y sus correspondientes funciones en js

---
+++ js/funciones.js
@@ -0,0 +1,62 @@
+function mostrarOcultarAntecedentes() {
+    var x = document.getElementById("mostrar_antecedentes");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mostrarOcultarOtros() {
+    var x = document.getElementById("mostrar_otros");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mostrarOcultarCursos() {
+    var x = document.getElementById("mostrar_cursos");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mostrarOcultarFormacion() {
+    var x = document.getElementById("mostrar_formacion");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mostrarOcultarDatos() {
+    var x = document.getElementById("mostrar_datos");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function webEmpresaX() {
+    var x = document.getElementById("web_empresa_x");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function webInstitutoX() {
+    var x = document.getElementById("web_instituto_x");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
